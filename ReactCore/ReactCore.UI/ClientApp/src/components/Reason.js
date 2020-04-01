7881fba27025abdfe68baaf8b0113df913ebaf77
Emijii <57643494+Emijii@users.noreply.github.com>
2020-04-01 12:36:16 -0600

    Added comments.

--- ReactCore/ReactCore.UI/ClientApp/src/components/Reason.js
+++ ReactCore/ReactCore.UI/ClientApp/src/components/Reason.js
@@ -1,6 +1,12 @@
 ﻿import * as React from 'react';
 import { Card, CardTitle, CardText, CardBody } from 'reactstrap';
 
+//This is an example of a stateless functional component.
+//The reason you would want to use a functional component over a class component is:
+//1. Easier to read and test.
+//2. You end up with less code.
+//3. Separating container and presentational components is a best practice. Presentational components don't need to set state.
+
 const Reason = ({ title, description }) => (
     <Card>
         <CardBody>
