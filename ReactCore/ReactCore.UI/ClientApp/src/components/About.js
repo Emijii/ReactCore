5e90229a8497f799f1bafed67ce1428c03029974
Emijii <57643494+Emijii@users.noreply.github.com>
2020-04-01 12:29:48 -0600

    Added Reason and About components. Updated routing. Removed extra code generated when the project was scaffolded.

---
+++ ReactCore/ReactCore.UI/ClientApp/src/components/About.js
@@ -0,0 +1,12 @@
+﻿import * as React from 'react';
+import { Card,  CardText, CardBody } from 'reactstrap';
+
+const About = () => (
+    <Card>
+        <CardBody>
+            <CardText>This application demonstrates the use of React with .NET Core.</CardText>
+        </CardBody>
+    </Card>
+)
+
+export default About;
---
+++ ReactCore/ReactCore.UI/ClientApp/src/components/Reason.js
@@ -0,0 +1,13 @@
+﻿import * as React from 'react';
+import { Card, CardTitle, CardText, CardBody } from 'reactstrap';
+
+const Reason = ({ title, description }) => (
+    <Card>
+        <CardBody>
+            <CardTitle>{title}</CardTitle>
+            <CardText>{description}</CardText>
+        </CardBody>
+    </Card>
+)
+
+export default Reason;
