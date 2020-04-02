fe2d760a6f867f1175ff3da9429ef76cb6156912
Emijii <57643494+Emijii@users.noreply.github.com>
2020-04-02 10:16:26 -0600

    Modified comment

--- ReactCore/ReactCore.DB/Security/reactcore.sql
+++ ReactCore/ReactCore.DB/Security/reactcore.sql
@@ -1,3 +1,4 @@
 ﻿--This is the schema we will use for the ReactCore tables.
---Creating a custom schema allows us to set security authorization settings by schema.
+--Creating a custom schema allows us to set security authorization settings by user.
+--So in this example, the [reactcore] schema's authorization is assigned to the ReactCore user.
 CREATE SCHEMA [reactcore] AUTHORIZATION [ReactCore]
