853b805152b44d1fab13db10110aa6d8950ce894
emily.jimmerson <57643494+Emijii@users.noreply.github.com>
2020-04-27 16:28:33 -0600

    Added React Code Snippet extension.

---
+++ ReactCore/ReactCore.UI/ClientApp/src/components/Snippet.js
@@ -0,0 +1,18 @@
+﻿import React, { PureComponent } from 'react';
+
+class Example extends PureComponent {
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
+    render() {
+        return (
+
+        );
+    }
+}
+
+export default Example;
+
+//This code was generated using the React Code Snippet extension.
+//Type [pccs] then hit the Tab key.
