1f323922e40337ec447a671f7ae65e8a16eaadd6
emily.jimmerson <57643494+Emijii@users.noreply.github.com>
2020-07-17 15:41:21 -0600

    Added button on carousel that navigates to the About page.

--- ReactCore/ReactCore.UI/ClientApp/src/components/CarouselDisplay.js
+++ ReactCore/ReactCore.UI/ClientApp/src/components/CarouselDisplay.js
@@ -1,61 +1,37 @@
-﻿import React, { useState, Fragment } from 'react';
-import { Carousel, CarouselItem, CarouselControl, CarouselIndicators, CarouselCaption } from 'reactstrap';
+﻿import React from 'react';
+import { Button, Carousel } from 'react-bootstrap';
+import { useHistory } from 'react-router-dom';
 import './CarouselDisplay.css';
 
-const items = [
-    {
-        src: './images/Flower.jpg',
-        altText: 'Flower 1',
-        caption: 'Flower 1'
-    },
-    {
-        src: './images/Flower.jpg',
-        altText: 'Flower 2',
-        caption: 'Flower 2'
-    },
-    {
-        src: './images/Flower.jpg',
-        altText: 'Flower 3',
-        caption: 'Flower 3'
-    }
-];
-
 const CarouselDisplay = (props) => {
-    const [activeIndex, setActiveIndex] = useState(0);
-    const [animating, setAnimating] = useState(false);
-
-    const next = () => {
-        if (animating) return;
-        const nextIndex = activeIndex === items.length - 1 ? 0 : activeIndex + 1;
-        setActiveIndex(nextIndex);
-    }
-
-    const previous = () => {
-        if (animating) return;
-        const nextIndex = activeIndex === 0 ? items.length - 1 : activeIndex - 1;
-        setActiveIndex(nextIndex);
-    }
-
-    const goToIndex = (newIndex) => {
-        if (animating) return;
-        setActiveIndex(newIndex);
-    }
-
-    const slides = items.map((item) => {
-        return (
-            <CarouselItem onExiting={() => setAnimating(true)} onExited={() => setAnimating(false)} key={item.src}>
-                <img src={item.src} alt={item.altText} />
-                <CarouselCaption captionText={item.caption} captionHeader={item.caption} />
-            </CarouselItem>
-        );
-    });
+    const history = useHistory();
 
     return (
-        <Carousel activeIndex={activeIndex} next={next} previous={previous}>
-            <CarouselIndicators items={items} activeIndex={activeIndex} onClickHandler={goToIndex} />
-            {slides}
-            <CarouselControl direction="prev" directionText="Previous" onClickHandler={previous} />
-            <CarouselControl direction="next" directionText="Next" onClickHandler={next} />
+        <Carousel>
+            <Carousel.Item>
+                <img src="./images/Flower.jpg" alt="First slide" />
+                <Carousel.Caption>
+                    <h3>Flower 1</h3>
+                    <p>Flower 1</p>
+                    <Button onClick={() => history.push("/About")}>Go to About Page</Button>
+                </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+                <img src="./images/Flower.jpg" alt="Third slide" />
+                <Carousel.Caption>
+                    <h3>Flower 2</h3>
+                    <p>Flower 2</p>
+                    <Button onClick={() => history.push("/About")}>Go to About Page</Button>
+                </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+                <img src="./images/Flower.jpg" alt="Third slide" />
+                <Carousel.Caption>
+                    <h3>Flower 3</h3>
+                    <p>Flower 3</p>
+                    <Button onClick={() => history.push("/About")}>Go to About Page</Button>
+                </Carousel.Caption>
+            </Carousel.Item>
         </Carousel>
     );
 }
