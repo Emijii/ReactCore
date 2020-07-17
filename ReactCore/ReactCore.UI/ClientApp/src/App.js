9f239a4dbdb5b52fddcdd8fe7a961a433889d99f
emily.jimmerson <57643494+Emijii@users.noreply.github.com>
2020-07-17 13:56:23 -0600

    Added Carousel example

--- ReactCore/ReactCore.UI/ClientApp/src/App.js
+++ ReactCore/ReactCore.UI/ClientApp/src/App.js
@@ -2,7 +2,8 @@ import React, { Component } from 'react';
 import { Route } from 'react-router';
 import { Layout } from './components/Layout';
 import { Home } from './components/Home';
-import  About from './components/About';
+import { Cards } from './components/Cards';
+import About from './components/About';
 
 import './custom.css'
 
@@ -13,6 +14,7 @@ export default class App extends Component {
         return (
             <Layout>
                 <Route exact path='/' component={Home} />
+                <Route path='/Cards' component={Cards} />
                 <Route path='/About' component={About} />
             </Layout>
         );
---
+++ ReactCore/ReactCore.UI/ClientApp/src/components/Cards.js
@@ -0,0 +1,33 @@
+﻿import React, { Component } from 'react';
+import { CardDeck } from 'reactstrap';
+import Reason from './Reason';
+
+export class Cards extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { reasons: [], loading: true };
+    }
+
+    componentDidMount() {
+        this.populateReasons();
+    }
+
+    render() {
+        return (
+            <CardDeck>
+                {
+                    this.state.reasons.map(reason => <Reason key={reason.id} summary={reason.summary} />)
+                }
+            </CardDeck>
+        );
+    }
+
+    async populateReasons() {
+        //TODO: Figure out CORS
+        //const response = await fetch('http://localhost:5000/reason');
+
+        const response = await fetch('http://localhost:50775/reason');
+        const data = await response.json();
+        this.setState({ reasons: data, loading: false });
+    }
+}
